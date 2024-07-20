13c252de3f7ac4241b17126c41e0dd6db3aea9e5
Gui5757 <buenovictorsantana@gmail.com>
2024-07-19 23:34:47 -0300

    Atividade do primeiro trimestre do melhor professor do universo

---
+++ Atividade-001-Varela-Victor Guilherme/assets/main.js
@@ -0,0 +1,33 @@
+window.alert("Para identificarmos que você não é um robo...")
+window.alert("Some mentalmente e dê o valor antes de aparecer!")
+var n1= "Hello world!"
+var n2= window.prompt('digite um numero')
+var n3= window.prompt('digite um numero')
+window.alert('a soma dos numeros é: '+ n1)
+
+
+setTimeout(function() {
+    var nome= window.prompt('Digite seu nome para identificarmos quem é: ')
+    
+    window.alert(`Olá ${nome} lindo, maravilhoso e hacker de html mais inteligente do world!!❤️`)
+
+
+
+},1)
+
+
+let bt = document.querySelector("button.b1")
+
+bt.addEventListener("click", function() {
+    window.open ( 'https://youtu.be/6DtOmSRtEA4');
+
+
+})
+
+
+
+
+
+
+
+
